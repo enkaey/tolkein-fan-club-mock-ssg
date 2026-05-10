b6caec75dd02a12d64819a905a3e79ce959fe6da
enkaey <nishantkamna@gmail.com>
2026-05-10 19:22:08 +0530

    feat: add TextNode class and unit tests
    - Define TextType using StrEnum (requires Python 3.11+)
    - Implement __eq__ and __repr__ for TextNode
    - Add initial test suite for equality and edge cases

---
+++ src/test_textnode.py
@@ -0,0 +1,25 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node_other = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node_other)
+    
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node_other = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node_other)
+    
+    def test_not_eq_url(self):
+        node = TextNode("This is a text node", TextType.LINK,)
+        node_other = TextNode("This is a text node", TextType.BOLD, "https://google.com")
+        self.assertNotEqual(node, node_other)
+    
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.CODE, "https://boot.dev")
+        self.assertEqual("TextNode(This is a text node, `, https://boot.dev)", repr(node))
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import StrEnum
+
+class TextType(StrEnum):
+    BOLD = "**"
+    ITALIC = "_"
+    CODE = "`"
+    LINK = "[]()"
+    IMG = "![]()"
+
+class TextNode:
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, TextNode):
+            return NotImplemented
+        return (self.text == other.text and
+                self.text_type == other.text_type and
+                self.url == other.url)
+    
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+    
